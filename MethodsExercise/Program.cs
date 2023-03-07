734a63583c11d44676a042a25a204a84a0e933e3
Alec Hancock <alec.m.hancock@gmail.com>
2023-03-06 20:13:40 -0600

    Assignment completed

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please give me a name of a pet.");
+            var pet = Console.ReadLine();
+
+            Console.WriteLine("Please give me your least favorite color.");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("Please give me a time of day, such as 12:30PM");
+            var time = Console.ReadLine();
+
+            Console.WriteLine("It was " + time + " on Saturday, when " + pet + " decided they would eat a " + color + " shirt.");
+
+            Console.WriteLine("We won't miss it much.");
+
         }
     }
 }
