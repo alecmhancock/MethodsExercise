600891386ec39f887cdbcb8116d5b721065710b1
Alec Hancock <alec.m.hancock@gmail.com>
2023-03-08 09:33:39 -0600

    Corrections to second exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -31,6 +31,23 @@
 
         }
 
+        public static void MadLib()
+        {
+            Console.WriteLine("Please give me a name of a pet.");
+            var pet = Console.ReadLine();
+
+            Console.WriteLine("Please give me your least favorite color.");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("Please give me a time of day, such as 12:30PM");
+            var time = Console.ReadLine();
+
+            Console.WriteLine("It was " + time + " on Saturday, when " + pet + " decided they would eat a " + color + " shirt.");
+
+            Console.WriteLine("We won't miss it much.");
+        }
+
+
         //variable definitions
 
         static void Main(string[] args)
@@ -56,18 +73,7 @@
 
             //userInput section
 
-            Console.WriteLine("Please give me a name of a pet.");
-            var pet = Console.ReadLine();
-
-            Console.WriteLine("Please give me your least favorite color.");
-            var color = Console.ReadLine();
-
-            Console.WriteLine("Please give me a time of day, such as 12:30PM");
-            var time = Console.ReadLine();
-
-            Console.WriteLine("It was " + time + " on Saturday, when " + pet + " decided they would eat a " + color + " shirt.");
-
-            Console.WriteLine("We won't miss it much.");
+            MadLib();
 
         }
 
